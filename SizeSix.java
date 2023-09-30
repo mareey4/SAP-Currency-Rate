7f04cc60cea04960955630b16c16cf6da5fc2661
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-09-30 21:23:29 +1300

    Add files via upload
    Required for the updated Exchange class for drawing the graphs

---
+++ SizeSix.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Currency_Exchange;
+
+/**
+ *
+ * @author snipi
+ */
+public class SizeSix extends Shapes{
+    
+    public SizeSix() {
+        super(6);
+        
+        super.nodes[0] = new Node("A", 130, 50);
+        super.nodes[1] = new Node("B", 310, 50);
+        super.nodes[2] = new Node("C", 420, 220);
+        super.nodes[3] = new Node("D", 310, 390);
+        super.nodes[4] = new Node("E", 130, 390);
+        super.nodes[5] = new Node("F", 30, 220);
+    }
+}
