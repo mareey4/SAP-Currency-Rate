d092c02196465aa70b63d114f3f66d5a917fc333
Maria Paule <132525779+mareey4@users.noreply.github.com>
2023-10-05 15:35:22 +1300

    Add files via upload

--- Graph.java
+++ Graph.java
@@ -7,6 +7,7 @@ package Currency_Exchange;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.util.Map;
 import javax.swing.JPanel;
 
 /**
@@ -16,72 +17,68 @@ import javax.swing.JPanel;
 public class Graph extends JPanel {
 
     protected final int dotSize = 25;
-    
     protected Shapes shape;
+    protected String[] labels; 
+
 
     public Graph(Shapes shape) {
         this.shape = shape;
     }
 
 @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        
-        for(Arrow current : shape.arrows) {
-            g.drawLine(current.fromX, current.fromY, current.toX, current.toY);
+public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.setColor(Color.BLACK);
+
+    // Calculate arrow positions based on the number of nodes
+    int numNodes = shape.nodes.length;
+
+    for (int i = 0; i < numNodes; i++) {
+        Node sourceNode = shape.nodes[i];
+        int x1 = sourceNode.x + dotSize / 2;
+        int y1 = sourceNode.y + dotSize / 2;
+
+        for (int j = 0; j < numNodes; j++) {
+            if (i != j) {
+                Node targetNode = shape.nodes[j];
+                int x2 = targetNode.x + dotSize / 2;
+                int y2 = targetNode.y + dotSize / 2;
+
+                // Calculate arrow direction (angle)
+                double angle = Math.atan2(y2 - y1, x2 - x1);
+
+                // Calculate arrow endpoints
+                int arrowX1 = (int) (x1 + (dotSize / 2) * Math.cos(angle));
+                int arrowY1 = (int) (y1 + (dotSize / 2) * Math.sin(angle));
+                int arrowX2 = (int) (x2 - (dotSize / 2) * Math.cos(angle));
+                int arrowY2 = (int) (y2 - (dotSize / 2) * Math.sin(angle));
+
+                // Draw a line from sourceNode to targetNode
+                g.drawLine(arrowX1, arrowY1, arrowX2, arrowY2);
+
+                // Draw an arrowhead at the targetNode
+                int arrowSize = 10; // Adjust this value as needed
+                int dx1 = (int) (arrowSize * Math.cos(angle - Math.PI / 6));
+                int dy1 = (int) (arrowSize * Math.sin(angle - Math.PI / 6));
+                int dx2 = (int) (arrowSize * Math.cos(angle + Math.PI / 6));
+                int dy2 = (int) (arrowSize * Math.sin(angle + Math.PI / 6));
+                g.drawLine(arrowX2, arrowY2, arrowX2 - dx1, arrowY2 - dy1);
+                g.drawLine(arrowX2, arrowY2, arrowX2 - dx2, arrowY2 - dy2);
+            }
         }
-
-//        for (int i = 0; i < shape.nodes.length; i++) {
-//            Node sourceNode = shape.nodes[i];
-//            int x1 = sourceNode.x + dotSize / 2;
-//            int y1 = sourceNode.y + dotSize / 2;
-//
-//            for (int j = 0; j < shape.nodes.length; j++) {
-//                if (i != j) {
-//                    Node targetNode = shape.nodes[j];
-//                    int x2 = targetNode.x + dotSize / 2;
-//                    int y2 = targetNode.y + dotSize / 2;
-//
-//                    int dx = x2 - x1;
-//                    int dy = y2 - y1;
-//
-//                    int perpDx = -dy;
-//                    int perpDy = dx;
-//
-//                    double length = Math.sqrt(perpDx * perpDx + perpDy * perpDy);
-//                    double scaleFactor = spacing / length;
-//                    perpDx *= scaleFactor;
-//                    perpDy *= scaleFactor;
-//
-//                    int arrowX1 = x1 + perpDx;
-//                    int arrowY1 = y1 + perpDy;
-//                    int arrowX2 = x2 + perpDx;
-//                    int arrowY2 = y2 + perpDy;
-//                    int arrowX3 = x1 - perpDx;
-//                    int arrowY3 = y1 - perpDy;
-//                    int arrowX4 = x2 - perpDx;
-//                    int arrowY4 = y2 - perpDy;
-//
-//                    g.drawLine(arrowX1, arrowY1, arrowX2, arrowY2);
-//                    drawArrowhead(g, x2, y2, arrowX1, arrowY1, arrowSize);
-//
-//                    g.drawLine(arrowX3, arrowY3, arrowX4, arrowY4);
-//                    drawArrowhead(g, x1, y1, arrowX3, arrowY3, arrowSize);
-//
-//                }
-//            }
-//        }
-
-        for (Node node : shape.nodes) {
-            g.drawOval(node.x, node.y, dotSize, dotSize);
-            Font labelFont = new Font("Calibri", Font.BOLD, 16);
-            g.setFont(labelFont);
-            g.drawString(node.name, (node.x + ((dotSize / 2) - 4)), (node.y + ((dotSize / 2) + 4)));
-        }
-
-        repaint();
     }
+
+    // Draw nodes
+    for (Node node : shape.nodes) {
+        g.drawOval(node.x, node.y, dotSize, dotSize);
+        Font labelFont = new Font("Calibri", Font.BOLD, 16);
+        g.setFont(labelFont);
+        g.drawString(node.name, (node.x + ((dotSize / 2) - 4)), (node.y + ((dotSize / 2) + 4)));
+    }
+
+    repaint();
+}
+
 private void drawArrowhead(Graphics g, int x1, int y1, int x2, int y2, int arrowSize) {
     double angle = Math.atan2(y2 - y1, x2 - x1);
     int x3 = (int) (x2 - arrowSize * Math.cos(angle - Math.PI / 6));
@@ -92,4 +89,7 @@ private void drawArrowhead(Graphics g, int x1, int y1, int x2, int y2, int arrow
     g.drawLine(x2, y2, x4, y4);
 }
 
+//public void setExchangeRates(Map<String, Map<String, String>> exchangeRates) {
+//        this.exchangeRates = exchangeRates;
+//    }
 }
