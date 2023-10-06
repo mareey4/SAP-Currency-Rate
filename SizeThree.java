7e762b95f5188553fbea84cc406489a1412170c0
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-10-06 23:36:00 +1300

    Update SizeThree.java

--- SizeThree.java
+++ SizeThree.java
@@ -16,47 +16,47 @@ public class SizeThree extends Shapes{
         super.nodes[1] = new Node("B", 370, 60);
         super.nodes[2] = new Node("C", 210, 320);
         
-        super.arrows[0] = new Arrow(
-                "AB",
-                super.nodes[1].x,
-                super.nodes[1].y,
-                super.nodes[0].x + super.dotSize,
-                super.nodes[0].y
-        );
-        super.arrows[1] = new Arrow(
-                "BA",
-                super.nodes[0].x + super.dotSize,
-                super.nodes[0].y + super.dotSize,
-                super.nodes[1].x,
-                super.nodes[1].y + super.dotSize
-        );
-        super.arrows[2] = new Arrow(
-                "BC",
-                super.nodes[2].x + super.dotSize,
-                super.nodes[2].y,
-                super.nodes[1].x + super.dotSize,
-                super.nodes[1].y + super.dotSize
-        );
-        super.arrows[3] = new Arrow(
-                "CB",
-                super.nodes[1].x,
-                super.nodes[1].y + super.dotSize,
-                super.nodes[2].x,
-                super.nodes[2].y
-        );
-        super.arrows[4] = new Arrow(
-                "CA",
-                super.nodes[0].x,
-                super.nodes[0].y + super.dotSize,
-                super.nodes[2].x,
-                super.nodes[2].y
-        );
-        super.arrows[5] = new Arrow(
-                "AC",
-                super.nodes[2].x + super.dotSize,
-                super.nodes[2].y,
-                super.nodes[0].x + super.dotSize,
-                super.nodes[0].y + super.dotSize
-        );
+        // super.arrows[0] = new Arrow(
+        //         "AB",
+        //         super.nodes[1].x,
+        //         super.nodes[1].y,
+        //         super.nodes[0].x + super.dotSize,
+        //         super.nodes[0].y
+        // );
+        // super.arrows[1] = new Arrow(
+        //         "BA",
+        //         super.nodes[0].x + super.dotSize,
+        //         super.nodes[0].y + super.dotSize,
+        //         super.nodes[1].x,
+        //         super.nodes[1].y + super.dotSize
+        // );
+        // super.arrows[2] = new Arrow(
+        //         "BC",
+        //         super.nodes[2].x + super.dotSize,
+        //         super.nodes[2].y,
+        //         super.nodes[1].x + super.dotSize,
+        //         super.nodes[1].y + super.dotSize
+        // );
+        // super.arrows[3] = new Arrow(
+        //         "CB",
+        //         super.nodes[1].x,
+        //         super.nodes[1].y + super.dotSize,
+        //         super.nodes[2].x,
+        //         super.nodes[2].y
+        // );
+        // super.arrows[4] = new Arrow(
+        //         "CA",
+        //         super.nodes[0].x,
+        //         super.nodes[0].y + super.dotSize,
+        //         super.nodes[2].x,
+        //         super.nodes[2].y
+        // );
+        // super.arrows[5] = new Arrow(
+        //         "AC",
+        //         super.nodes[2].x + super.dotSize,
+        //         super.nodes[2].y,
+        //         super.nodes[0].x + super.dotSize,
+        //         super.nodes[0].y + super.dotSize
+        // );
     }
 }
