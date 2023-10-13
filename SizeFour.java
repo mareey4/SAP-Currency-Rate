59854f57c9c697500259bdbdb8f8d865e5330c8d
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-10-13 23:42:41 +1300

    Update SizeFour.java
    Updated to have coordinates of 'labels' array for drawing the exchange rates labels in CurrencyGraph and NegativeGraph classes

--- SizeFour.java
+++ SizeFour.java
@@ -15,9 +15,24 @@ public class SizeFour extends Shapes{
     public SizeFour() {
         super(4);
         
+        // Node coordinates
         super.nodes[0] = new Node("A", 80, 80);
         super.nodes[1] = new Node("B", 360, 80);
         super.nodes[2] = new Node("C", 360, 360);
         super.nodes[3] = new Node("D", 80, 360);
+        
+        // Exchange rates label coordinates
+        super.labels[0] = new Node("A to B: ", 50, 45);
+        super.labels[1] = new Node("A to C: ", 50, 60);
+        super.labels[2] = new Node("A to D: ", 50, 75);
+        super.labels[3] = new Node("B to A: ", 330, 45);
+        super.labels[4] = new Node("B to C: ", 330, 60);
+        super.labels[5] = new Node("B to D: ", 330, 75);
+        super.labels[6] = new Node("C to A: ", 330, 400);
+        super.labels[7] = new Node("C to B: ", 330, 415);
+        super.labels[8] = new Node("C to D: ", 330, 430);
+        super.labels[9] = new Node("D to A: ", 50, 400);
+        super.labels[10] = new Node("D to B: ", 50, 415);
+        super.labels[11] = new Node("D to C: ", 50, 430);
     }
 }
