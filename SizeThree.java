b044b9c3f3d47bd3b54bf33733d45439f6fb39ff
Karl Francisco <134113180+Ph4nt45m@users.noreply.github.com>
2023-10-13 23:43:07 +1300

    Update SizeThree.java
    Updated to have coordinates of 'labels' array for drawing the exchange rates labels in CurrencyGraph and NegativeGraph classes

--- SizeThree.java
+++ SizeThree.java
@@ -15,8 +15,17 @@ public class SizeThree extends Shapes{
     public SizeThree() {
         super(3);
         
+        // Node coordinates
         super.nodes[0] = new Node("A", 50, 60);
         super.nodes[1] = new Node("B", 370, 60);
         super.nodes[2] = new Node("C", 210, 320);
+        
+        // Exchange rates label coordinates
+        super.labels[0] = new Node("A to B: ", 20, 35);
+        super.labels[1] = new Node("A to C: ", 20, 50);
+        super.labels[2] = new Node("B to A: ", 340, 35);
+        super.labels[3] = new Node("B to C: ", 340, 50);
+        super.labels[4] = new Node("C to A: ", 180, 360);
+        super.labels[5] = new Node("C to B: ", 180, 375);
     }
 }
